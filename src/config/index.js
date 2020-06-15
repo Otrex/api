14386d7b054018b083e7696079e8b4884ab289a8
Alor Tochukwu <alortochukwu@gmail.com>
2020-06-15 10:03:00 +0100

    tests

--- src/config/index.js
+++ src/config/index.js
@@ -8,16 +8,16 @@ const config = {
   },
   app: {
     bcryptRounds: 10,
-    jwtSigningKey: process.env.JWT_SIGNING_KEY || "DEADBOLT",
-    port: process.env.PORT || "5678"
+    jwtSigningKey: process.env.JWT_SIGNING_KEY,
+    port: process.env.PORT
   },
   db: {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "27017",
-    database: process.env.DB_DATABASE || "pointograph",
-    user: process.env.DB_USER ||"root",
-    password: process.env.DB_PASSWORD ||"root",
-    authSource: process.env.DB_AUTH_SOURCE || "admin"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    authSource: process.env.DB_AUTH_SOURCE
   }
 };
 
