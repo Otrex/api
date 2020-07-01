c7a9178ff905cf88e4e3407016fe5da35b4ae2d9
Alor Tochukwu <alortochukwu@gmail.com>
2020-07-01 17:30:17 +0100

    populate territories don

--- pointograph.js
+++ pointograph.js
@@ -8,18 +8,18 @@ db.createConnection().then(async () => {
     console.log(`listening on port: ${config.app.port}`);
   });
   // populate territories
-  const countries = require("./src/lib/countries.json");
-  for (const feature of countries.features) {
-    if (feature.properties.ADMIN === "Antarctica") {
-      continue;
-    }
-    await db.models.Territory.create({
-      name: feature.properties.ADMIN,
-      description: feature.properties.ADMIN,
-      properties: feature.properties,
-      geometry: feature.geometry
-    }).catch(e => console.error("error", e) && process.exit(1));
-    console.log("done for: ", feature.properties.ADMIN);
-  }
+  // const countries = require("./src/lib/countries.json");
+  // for (const feature of countries.features) {
+  //   if (feature.properties.ADMIN === "Antarctica") {
+  //     continue;
+  //   }
+  //   await db.models.Territory.create({
+  //     name: feature.properties.ADMIN,
+  //     description: feature.properties.ADMIN,
+  //     properties: feature.properties,
+  //     geometry: feature.geometry
+  //   }).catch(e => console.error("error", e) && process.exit(1));
+  //   console.log("done for: ", feature.properties.ADMIN);
+  // }
 });
 
