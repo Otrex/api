6e583cf484f135b1a16f20596de30258451b71e1
Alor Tochukwu <alortochukwu@gmail.com>
2020-07-19 21:22:12 +0100

    fix

--- src/tasks/seedTerritories.js
+++ src/tasks/seedTerritories.js
@@ -1,6 +1,10 @@
+const db = require("../db");
 const TerritoryService = require("../services/Territory");
 
 (async function () {
+  await db.createConnection();
+  console.log("[INFO] connected to DB");
+
   console.log("--> Populating NGA Territories");
   const data = require("../data/territories/NGA/gadm36_NGA_0.geo.json");
   const data1 = require("../data/territories/NGA/gadm36_NGA_1.geo.json");
